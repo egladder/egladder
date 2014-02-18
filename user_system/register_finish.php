0d33991781393e854bb697112758cffc0629bf56
Jhongjhu <yuuhi1379456@gmail.com>
2014-02-18 23:04:19 +0800

    fix

--- user_system/register_finish.php
+++ user_system/register_finish.php
@@ -13,6 +13,27 @@
         return $email; 
     }
 
+if($_SESSION['username'] != null)
+{
+        //將$_SESSION['username']丟給$id
+        //這樣在下SQL語法時才可以給搜尋的值
+        $id = $_SESSION['username'];
+        //若以下$id直接用$_SESSION['username']將無法使用
+        $sql = "SELECT * FROM members_table where username='$id'";
+        $result = mysql_query($sql);
+        $row = mysql_fetch_row($result);
+    
+        echo "<form name=\"form\" method=\"post\" action=\"update_finish.php\">";
+        echo "email：<input type=\"text\" name=\"id\" value=\"$row[1]\" />(此項目無法修改) <br>";
+        echo "password：<input type=\"password\" name=\"pw\" value=\"$row[2]\" /> <br>";
+        echo "<input type=\"submit\" name=\"button\" value=\"確定\" />";
+        echo "</form>";
+}
+else
+{
+        echo '您無權限觀看此頁面!';
+        echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
+}
 
     $id = a( $_POST['email'] );
     $pw = $_POST['pw'];
