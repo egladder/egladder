60278249f5bd69bc9fdc47b82aa985bb62d3e42e
Jhongjhu <yuuhi1379456@gmail.com>
2014-02-20 22:11:25 +0800

    fix

--- user_system/deleteuser.php
+++ user_system/deleteuser.php
@@ -1,3 +1,4 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php 
 	include("mysql_connection.php");
     
@@ -6,6 +7,6 @@
     //$row = mysql_fetch_row($result);
 
         while ($row = mysql_fetch_row($result)) {
-            echo "<p>Email: $row[1], PW: $row[2] <a href='deleteuser.php'>delete</a></p>"; 
+            echo "<p>Email: $row[1], PW: $row[2] <a href='deletefun.php?pw=$row[2]'>delete</a></p>"; 
         }
 ?>
